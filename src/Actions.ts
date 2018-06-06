357325ffe59fbb6fb57730e233a784d786338b4c
Paul Everitt <pauleveritt@me.com>
2018-06-06 11:50:35 -0400

    Provide more typing and use of the ActionsType.

--- src/Actions.ts
+++ src/Actions.ts
@@ -1,6 +1,13 @@
+import { ActionsType } from "hyperapp";
+
 import IState from "./IState";
 
-export default class Actions {
-    down = (value: number) => (state: IState) => ({ count: state.count - value });
-    up = (value: number) => (state: IState) => ({ count: state.count + value });
+export interface IActions {
+    up: (value: number) => Partial<IState>;
+    down: (value: number) => Partial<IState>;
+}
+
+export default class Actions implements ActionsType<IState, IActions> {
+    down = (value: number) => (state: IState) => ({count: state.count - value});
+    up = (value: number) => (state: IState) => ({count: state.count + value});
 }
